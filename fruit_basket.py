86dcc9180fce1a28f6dca584a2053deb58f8f53b
lthomas <rhytmring619@gmail.com>
2018-01-18 22:20:56 -0500

    modified the functionality

--- fruit_basket.py
+++ fruit_basket.py
@@ -1,5 +1,10 @@
+def guess_fruit(fruit_basket):
+	guess = raw_input('Guess a fruit from the basket:')
+	if guess in fruit_basket:
+		print('Your Guess is correct!')
 
-fruit_basket = ['apple','banana','orange','avocado','blueberry']
-guess = raw_input('Guess a fruit from the basket:')
-if guess in fruit_basket:
-	print('Your Guess is correct!')
+def main():
+	fruit_basket = ['apple','banana','orange','avocado','blueberry']
+	guess_fruit(fruit_basket)
+
+main()
